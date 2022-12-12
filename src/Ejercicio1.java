e9002a4fbd2ccfcad9a7691217942b9864adc5c2
ivamar <skrillexgamesdef@gmail.com>
2022-12-12 16:54:01 +0100

    Ejercicio1 casi hecho

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -10,9 +10,42 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         
+        int[][]datos = new int [5][7];
         
         
-        
+        //BUCLE PARA ASIGNAR VALORES CON RANDOM
+        for (int i = 0; i < datos.length; i++) {//AQUI ENTRAMOS EN LA PRIMERA FILA
+            for (int j = 0; j <datos[i].length ; j++) {// AQUI RECORREMOS LA FILA COMO TAL//ESTOS DOS BUCLES SON SIMPLEMENTE PARA ASIGNAR LOS VALORES CON RANDOM
+                datos[i][j]= (int) (Math.random()*35+0);
+                
+               
+            }
+            
+        }
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.println("Visualizamos la matriz COMPLETA: en la fila "+i+" y columna: "+j+" tenemos el valor :  "+datos[i][j]);
+                
+            }
+            
+        }
+        System.out.println("************************************");
+        for (int i = 4; i < datos.length; i++) {// INDICAMOS EN LA i LA POSICION QUE QUEREMOS SOLAMENTE IMPRIMIR
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.println("Visualizamos la fila 5 completa: "+datos[i][j]);
+                
+            }
+            
+        }
+        System.out.println("************************************");
+        for (int i = 0; i < datos.length; i++) {
+            
+        }
+
     }
     
 }
+
+
+//int matriz[][] = new int[(int) (Math.random()*9+1)][(int) (Math.random()*9+1)];
+//ESTA SERÍA OTRA FORMA DE HACERLO
